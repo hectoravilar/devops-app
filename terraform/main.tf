2c5d5ed90052b3fd1580bbb296cda111bbd5d944
Hector Avila <hectoravllr@gmail.com>
2026-03-20 19:08:12 -0300

    feat: add main SQS queue and Dead Letter Queue

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,33 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "docflow_bucket" {
   bucket = "docflow-pdfs-hector-dev"
 
   tags = {
     Name        = "My bucket"
+    Environment = "DevOps"
+  }
+}
+resource "aws_sqs_queue" "docflow_dlq" {
+  name = "docflow-dlq"
+
+  tags = {
+    Environment = "DevOps"
+  }
+}
+resource "aws_sqs_queue" "docflow_queue" {
+  name                       = "docflow-sqs"
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  visibility_timeout_seconds = 60
+  receive_wait_time_seconds  = 10
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.docflow_dlq.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
     Environment = "DevOps"
   }
 }
